86bc1d156afacca8db25bcb9361f336828cd2dc2
Sergey Kravchenko <skravchenko223@gmail.com>
2023-08-02 04:56:05 +0200

    добавление компонента и библиотеки

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,11 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import ContactsItem from 'components/contactsItem/ContactsItem';
 import css from './contacts.module.css';
-import { deleteContact } from 'redux/phoneBookSlice';
 
 const Contacts = () => {
   const contactsArray = useSelector(getContacts);
-  const dispatch = useDispatch();
   const normilizedFilter = useSelector(getFilter).toLowerCase();
   const filterContacts = contactsArray.filter(contact =>
     contact.nameUser.toLowerCase().includes(normilizedFilter)
@@ -15,16 +14,12 @@ const Contacts = () => {
     <ul className={css.contactsList}>
       {filterContacts.map(contact => {
         return (
-          <li className={css.contactsItem} key={contact.id}>
-            {contact.nameUser}: {contact.number}
-            <button
-              className={css.btnDelete}
-              onClick={() => dispatch(deleteContact(contact.id))}
-              type="button"
-            >
-              Delete
-            </button>
-          </li>
+          <ContactsItem
+            key={contact.id}
+            id={contact.id}
+            nameUser={contact.nameUser}
+            number={contact.number}
+          />
         );
       })}
     </ul>
